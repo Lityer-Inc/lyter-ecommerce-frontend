65554f0a717caea3a9422f6564229c00a629ce36
saadabban76 <saadabban76@gmail.com>
2024-01-10 20:43:59 +0530

    login pop-op appears when user is not logged in on click of checkout button

--- src/Components/Checkout.jsx
+++ src/Components/Checkout.jsx
@@ -1,16 +1,66 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { ShopContext } from "../context/shop-context";
 import CartItem from "../Components/CartItem";
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
 
 export const CheckoutPage = ({ isCartOpen2, toggleMobileCart }) => {
-  const { cartItems, addToCart, removeFromCart, totalItems, totalPrice } = useContext(ShopContext);
+  const {
+    cartItems,
+    setProductSelected,
+    addToCart,
+    removeFromCart,
+    totalItems,
+    totalPrice,
+    setLoginModal,
+    setAlert,
+    setAlertState
+  } = useContext(ShopContext);
+
+  const token = Cookies.get("token") ? JSON.parse(Cookies.get("token")) : null;
+  const navigate = useNavigate();
 
   const handleResize = () => {
     const width = window.innerWidth;
     if (isCartOpen2 && width < 766) {
-      toggleMobileCart(false)
+      toggleMobileCart(false);
+    }
+  };
+
+  const checkoutHandler = async () => {
+    if (token != null) {
+      // user is logged in
+      navigate('/about', {replace: true});
+      toggleMobileCart(false);
+    } else {
+      toggleMobileCart(false);
+      setAlert(true);
+      setAlertState({
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+          >
+            <g fill="none" stroke="currentColor" strokeLinejoin="round">
+              <path
+                strokeWidth="2"
+                d="M2 14.5A4.5 4.5 0 0 0 6.5 19h12a3.5 3.5 0 0 0 .5-6.965a7 7 0 0 0-13.76-1.857A4.502 4.502 0 0 0 2 14.5Z"
+              />
+              <path strokeWidth="3" d="M12 15.5h.01v.01H12z" />
+              <path strokeLinecap="round" strokeWidth="2" d="M12 12V9" />
+            </g>
+          </svg>
+        ),
+        status: "Checkout Failed",
+        msg1: "Please Login Before Checking Out the Products",
+        // msg2: `Reason: ${res.error}`,
+        action: "Retry"
+      });
+      setTimeout(() => {
+        setLoginModal(true);
+      }, 2000);
     }
   };
 
@@ -23,8 +73,17 @@ export const CheckoutPage = ({ isCartOpen2, toggleMobileCart }) => {
   }, []);
 
   return (
-    <main className={`fixed flex w-full h-full top-0 left-0 z-30 justify-center ${isCartOpen2 ? "" : "hidden"}`}>
-      <div className=" absolute bg-black/20 w-full h-full" onClick={() => { toggleMobileCart(false) }} />
+    <main
+      className={`fixed flex w-full h-full top-0 left-0 z-30 justify-center ${
+        isCartOpen2 ? "" : "hidden"
+      }`}
+    >
+      <div
+        className=" absolute bg-black/20 w-full h-full"
+        onClick={() => {
+          toggleMobileCart(false);
+        }}
+      />
       <div id="cart-items" className="z-40 self-center w-[90%]">
         {cartItems ? (
           <div className="bg-[#fff] rounded-lg overflow-x-auto md:overflow-x-hidden h-100 md:h-auto p-2 md:p-10 flex flex-col justify-center items-center gap-5 shadow-lg">
@@ -65,21 +124,19 @@ export const CheckoutPage = ({ isCartOpen2, toggleMobileCart }) => {
                     if (index > 5) {
                       return;
                     }
-                    return (
-                      <CartItem data={item} key={index} />
-                    );
+                    return <CartItem data={item} key={index} />;
                   })}
                 </tbody>
               </table>
             </div>
             <div className=" w-full border-1 border-[#f5f5f5] flex justify-between p-5">
-            <div className="">Total</div>
+              <div className="">Total</div>
 
-            <div className="">{totalPrice}</div>
-          </div>
+              <div className="">{totalPrice}</div>
+            </div>
             <Link
-              to={'/checkout-payment'}
-              onClick={() => { toggleMobileCart(false) }}
+              to={`${token!=null ? 'checkout-payment' : ""}`}
+              onClick={checkoutHandler}
               className="bg-[#FF0066] max-w-[200px] transition-all hover:bg-[#ff0f6f] text-center 
           py-3 mb-2 mr-2 self-end rounded-md font-bold text-white
           w-full h-12"
