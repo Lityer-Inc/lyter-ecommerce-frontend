b24d54b3e552b4568e6d965b8bbe304761f20251
mohammadashrafulislam <ixdell41@gmail.com>
2024-01-17 22:51:40 +0600

    added [cetagory: Beverages

--- src/Pages/Beverages.jsx
+++ src/Pages/Beverages.jsx
@@ -1,9 +1,37 @@
-import React from "react";
-import data from "../DummyData/data.js";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import Card from "../Components/Card";
 import { nanoid } from "nanoid";
-export default function Beverages({ img, title, description, price }) {
-  const beveragesData = data.find((data) => data.name === "beverage");
+import { ShopContext } from "../context/shop-context";
+import { useSearchParams } from "react-router-dom";
+
+export default function Beverages() {
+  const { endpointHead } = useContext(ShopContext);
+  const [params] = useSearchParams();
+
+  const storeId = useRef(params.get("id"));
+  const [beveragesData, setBeveragesData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `${endpointHead}/stores/${storeId.current}/products`
+        );
+        const data = await response.json();
+        setBeveragesData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [endpointHead, storeId]);
+
+  // Filter beverages based on the category
+  const filteredBeverages = beveragesData.filter(
+    (item) => item.category === "Beverages"
+  );
+
   return (
     <div className="page-wrapper">
       <div className="beverages-page-container category-page-container">
@@ -11,18 +39,12 @@ export default function Beverages({ img, title, description, price }) {
           Cool off this summer with these&nbsp;<u>ice-cold</u>&nbsp;beverages.
         </div>
       </div>
-      <div className="beverages-items category-items">
-        {
-          <div key={nanoid()}>
-            <Card
-              img={beveragesData.img}
-              title={beveragesData.title}
-              description={beveragesData.description}
-              price={beveragesData.price}
-              id={beveragesData.id}
-            />
+      <div className="beverages-items grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-10 category-items mt-12">
+        {filteredBeverages.map((item) => (
+          <div key={nanoid()} className="grid-item">
+            <Card data={item} />
           </div>
-        }
+        ))}
       </div>
     </div>
   );
